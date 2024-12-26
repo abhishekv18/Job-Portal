d78d1295a04751238882626e9b94089b1643dc3b
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2024-12-26 23:04:37 +0530

    change made

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -34,7 +34,7 @@ const Jobs = () => {
         filterJobs.length<=0?<span>Job not found</span>:(<div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
           <div className='grid grid-cols-3 gap-4'>
           {
-  allJobs.map((job)=>(<motion.div
+  filterJobs.map((job)=>(<motion.div
     initial={{ opacity: 0, x: 100 }}
     animate={{ opacity: 1, x: 0 }}
     exit={{ opacity: 0, x: -100 }}
