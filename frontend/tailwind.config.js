17a5411015108f8415a2100f9c48d4240e5bfac9
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2025-01-25 00:29:52 +0530

    require error

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+
+import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -53,6 +55,7 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
+ // plugins: [require("tailwindcss-animate")],
 }
 
